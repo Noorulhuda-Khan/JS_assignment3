9f383d3d272d331db257740264172dd42c185d84
Noorulhuda Khan <hmnoorulhuda2014@gmail.com>
2023-08-21 21:55:52 +0500

    Q6 tak

---
+++ app.js
@@ -0,0 +1,44 @@
+// Q1
+
+var studentsName=[];
+
+// Q2
+
+// n/a
+
+
+
+// Q3
+
+// var ary =["abc"];
+
+// Q4
+// var ary1=[1, 2];
+
+
+// Q5
+
+// var ary2= [true, false]
+
+
+
+// Q6
+
+// var ary3=[1, "abc"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

e05b593ecb0f113caf828aaa8697ee7f0c1bf1f6
Noorulhuda Khan <hmnoorulhuda2014@gmail.com>
2023-08-25 22:21:33 +0500

    loops

--- app.js
+++ app.js
@@ -55,26 +55,83 @@ var studentsName=[];
 // console.log(ali)
 
 
+// var students=["ahmed", "ahsan", "ammar", "kashif"]
+
+
+// var ali = students.unshift("faisal");.....shift pop ka opposite hy, unshift opposite hy push ka
+
+// console.log(ali)
+
+
+// var quali= ["1) SSC","2) HSC", "3) BE", "4) ME"]
+
+// alert("Qulifications"+ 
+// quali[0]
+// +quali[1]
+// +quali[2]
+// +quali[3]
+
+// )
+
+
+// LOOPS
+
+// for (var i=0; i < 5; i++ ){
+// console.log("Test")
+
+// }
+
+// for (var i=0; i <= 5; i++ ){
+// console.log("Test")
+
+// }
 
 
 
 
+// for (var i=0; i <= 5; i++ ){
+// console.log(i)
+
+// }
 
 
+// var arr=["ali","ahmed", "khan"]
+
+// var flag = false
+
+// for (var i =0; i> arr.length; i++){
+// if (arr[i]== "ahmed"){
+// flag="yes";
+// console.log(flag)
+// }
+// }
+
+// var num1=[1,2,3,4,6,7]
+
+// for (var i=0; i <=7; i++ ){
+
+//     var num2= num1[i]-1
+//     if (num2-1 > 1){
+//         break;
+//         console.log(num2)
+//     }
+// // console.log(num2)
+
+// }
+
+var numbers= [13, 14,15,17,18,19,21]
+
+for (var i=0 ; i < numbers.length-1; i++){
+
+// numbers[i+1]+"-"+numbers[i];
 
 
+if (numbers[i+1]-numbers[i] !== 1){
 
+    console.log(numbers[i]+1)
+}
 
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
